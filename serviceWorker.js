190eaf11fd4c83c51c94fbeb814eb8c526430bb8
Ivan Yancharkin <yancharkin@gmail.com>
2024-08-02 00:12:51 +0400

    Upgrade to PWA

---
+++ serviceWorker.js
@@ -0,0 +1,14 @@
+const cacheName = 'PWA-v1'
+
+self.addEventListener('fetch', (e) => {
+    e.respondWith((async () => {
+        const r = await caches.match(e.request);
+        console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+        if (r) return r;
+        const response = await fetch(e.request);
+        const cache = await caches.open(cacheName);
+        console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+        cache.put(e.request, response.clone());
+        return response;
+    })());
+});
